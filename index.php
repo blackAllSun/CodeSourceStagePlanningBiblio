f1e59347f91b2c7b48c25082bbdfea31b6af65f6
Le Hegaret Viviane <attilavlh10@gmail.com>
2021-06-25 19:22:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
   <meta charset="utf-8">
   <title>clone demo</title>
   <script src="jquery-3.5.1.min.js"></script>
+  <style>
+.add[data-id='1'] {display:none;}
+.add[data-id='2'] {display:none;}
+.add[data-id='3'] {display:none;}
+.add[data-id='4'] {display:none;}
+</style>
 </head>
 <body>
  <table>
@@ -19,31 +25,42 @@ foreach($data as $d) {
     }*/
 } ?>
 </table>
+
 <script>
-
 $(document).ready(function(){
+	$('.add').on({
+	    mouseenter: function(){
+	      $(this).css("background-color", "blue");
+	     }, 
+	    mouseleave: function(){
+	       $(this).css("background-color", "red");
+	    }, 
+	    click: function(e){
+	      $(this).css("background-color", "yellow");
+	    	var dataId = $(this).data("id");
+	    	var increment =dataId+1;
+	    	console.log("dataId:after click "+dataId);
+	    	console.log("increment:after click "+increment);
+	    	$('.add').fadeOut(400);
+	    	console.log("dataId:after fadeOut "+dataId);
+	    	console.log("increment:after fadeOut "+increment);
+	    	var temp=$("tr[class=attr"+ dataId +"]").clone();
+	    	console.log(temp);
+	    	temp.attr('class', "attr"+ increment);
+	    	temp.appendTo($('table'));
+	    	console.log("dataId:after clone "+dataId);
+	    	console.log("increment:after clone "+increment);
+	    	//recuperer data-id du bouton créé
+	    	
+	    	var temp2=$(".add[data-id="+ dataId +"]");
+	    	console.log("test temp2 "+temp2);
+	    	temp2.attr('data-id',increment);
+	    	console.log("test temp2 after "+temp2);
+
+	    } 
+	  });
 
 
-    $('.add').on("click",function(e){ 
-    	var dataId = $(this).data("id");
-    	var increment =dataId+1;
-    	console.log("dataId:after click "+dataId);
-    	console.log("increment:after click "+increment);
-    	$('.add').fadeOut(400);
-    	console.log("dataId:after fadeOut "+dataId);
-    	console.log("increment:after fadeOut "+increment);
-    	var temp=$("tr[class=attr"+ dataId +"]").clone();
-    	console.log(temp);
-    	temp.attr('class', "attr"+ increment);
-    	//recuperer data-id du bouton créé
-    	var temp2=$(".add[data-id="+ dataId +"]");
-    	console.log("test temp2 "+temp2);
-    	temp2.attr('data-id',increment);
-    	console.log("test temp2 after "+temp2);
-    	temp.appendTo($('table'));
-    	console.log("dataId:after clone "+dataId);
-    	console.log("increment:after clone "+increment);
-    });
 });
 
 </script>
