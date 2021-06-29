69978d4d7aad1f1f72cbef48508cab9296955e86
Le Hegaret Viviane <attilavlh10@gmail.com>
2021-06-29 13:05:17 +0200

    Update testAbsence.php

--- testAbsence.php
+++ testAbsence.php
@@ -1,9 +1,13 @@
 <?php
+ini_set('display_errors','on');
+error_reporting(E_ALL);
 /**
 
 Fatal error: Uncaught Error: Cannot access protected property App\Model\Absence::$perso_id in /home/planning/www/planningbiblio/public/test.php:12 Stack trace: #0 {main} thrown in /home/planning/www/planningbiblio/public/test.php on line 12
 **/
-require_once __DIR__.'/../autoload.php';
+
+
+require_once __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/init.php';
 use App\Model\Absence;
 
@@ -13,3 +17,5 @@ $absence=new Absence();
 $absence->perso_id(1);
 echo $absence->perso_id;
 var_dump($absence);
+
+echo 'test';
