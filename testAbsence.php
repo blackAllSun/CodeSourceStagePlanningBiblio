41bd838eecd9a372f110a3a71fb925d2a02ee5f9
Le Hegaret Viviane <attilavlh10@gmail.com>
2021-06-29 15:32:51 +0200

    Update testAbsence.php

--- testAbsence.php
+++ testAbsence.php
@@ -2,41 +2,39 @@
 ini_set('display_startup_errors', 1);
 ini_set('display_errors', 1);
 error_reporting(-1);
-/**
-Fatal error: Uncaught Error: Call to a member function persist() on array in /home/planning/www/planningbiblio/public/test.php:38 Stack trace: #0 {main} thrown in /home/planning/www/planningbiblio/public/test.php on line 38
-
-Fatal error: Uncaught Error: Cannot access protected property App\Model\Absence::$perso_id in /home/planning/www/planningbiblio/public/test.php:12 Stack trace: #0 {main} thrown in /home/planning/www/planningbiblio/public/test.php on line 12
-**/
 
 require_once __DIR__.'/../vendor/autoload.php';
 require_once(__DIR__.'/init.php');
 use App\Model\Absence;
 
-$em=$entityManager->getRepository(Absence::class)->findBy(array('perso_id' => 1));
 $absence=new Absence();
 $absence->perso_id(1);
-$absence->debut('2021-07-27 23:59:59.000000');
-$absence->fin('2021-07-30 23:59:59.000000');
+$absence->debut(new \DateTime());
+$absence->fin(new \DateTime());
 $absence->motif('maladie');
 $absence->motif_autre('contagieuse');
 $absence->commentaires('covid-19');
 $absence->etat('contagieux');
-$absence->demande('2021-07-30 23:59:59.000000');
+$absence->demande(new \DateTime());
 $absence->valide(1);
-$absence->validation('2021-07-30 23:59:59.000000');
+$absence->validation(new \DateTime());
 $absence->valide_n1(2);
-$absence->validation_n1('2021-07-30 23:59:59.000000');
+$absence->validation_n1(new \DateTime());
 $absence->pj1(1);
 $absence->pj2(1);
 $absence->so(6);
 $absence->groupe('test');
 $absence->cal_name('test');
 $absence->ical_key('');
-$absence->last_modified('2021-07-30 23:59:59.000000');
+$absence->last_modified('');
 $absence->uid('');
 $absence->rrule('');
 $absence->id_origin(1);
-$em->persist($absence);
-$em->flush();
-var_dump($absence);
+$entityManager->persist($absence);
+$entityManager->flush();
 
+$absenceRepository = $entityManager->getRepository(Absence::class);
+$absences=$absenceRepository->findAll();
+foreach($absence as $absences){
+    var_dump($absence);
+}
